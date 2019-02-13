887c0daf3bca297dc190378d73770a636bea3b5f
erziealdrian02 <erzie.aldrian02@gmail.com>
2019-02-13 09:52:20 +0700

    ==========================================================================
    --------------------------------------------------------
    |Suportifo_userfrontend_re       |
    |Pengedit : Erzie Aldrian       |
           |   email : erzie.aldrian02@gmail.com              |
    |No tlp/wa : 081221237531       |
    |github : https://github.com/erziealdrian02     |
    --------------------------------------------------------
    
        Hubungi saya jika ada pertanyaan
    
    ==========================================================================

---
+++ application/controllers/Suportivo.php
@@ -0,0 +1,19 @@
+<?php
+
+ class Suportivo extends CI_Controller {
+
+ 	function __contructs(){
+
+ 		$this->load->model('GambarModel');
+
+	}
+
+ 	public function page(){
+ 		echo "ini coba muncul apa engga";
+ 	}
+
+ 	public function index(){    
+ 		$this->load->view('v-landingpage');
+ 	}
+}
+?>
